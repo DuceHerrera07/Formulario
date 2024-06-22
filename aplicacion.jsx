8f783bd991361f4bd12c5cb5ac49357fc36e530d
DulceHerrera07 <hgdulce267@gmail.com>
2024-06-22 15:33:03 -0600

    cambio de nombre al .css e importacion de librerias a usar

---
+++ aplicacion.jsx
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import './aplicacion.css';
+
+export default function aplicacion() {
+  return (
+    <div>aplicacion</div>
+  )
+}
