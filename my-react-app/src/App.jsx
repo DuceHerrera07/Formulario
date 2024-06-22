e2cce3e46351c91d66919b6893694c5a424a5621
DulceHerrera07 <hgdulce267@gmail.com>
2024-06-22 16:15:54 -0600

    estilos y salto de linea

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -19,24 +19,28 @@ function App() {
         <div className="form-group">
           <label htmlFor="fullName" className="dark-label">Nombre Completo:</label>
           <input id="fullName" className="dark-input" {...register('fullName', { required: true })} />
+          <br/>
           {errors.fullName && <span className="dark-error">El nombre completo es obligatorio</span>}
         </div>
 
         <div className="form-group">
           <label htmlFor="email" className="dark-label">Correo Electrónico:</label>
           <input id="email" className="dark-input" type="email" {...register('email', { required: true })} />
+          <br/>
           {errors.email && <span className="dark-error">El correo electrónico es obligatorio</span>}
         </div>
 
         <div className="form-group">
           <label htmlFor="subject" className="dark-label">Asunto:</label>
           <input id="subject" className="dark-input" {...register('subject', { required: true })} />
+          <br/>
           {errors.subject && <span className="dark-error">El asunto es obligatorio</span>}
         </div>
 
         <div className="form-group">
           <label htmlFor="message" className="dark-label">Mensaje:</label>
           <textarea id="message" className="dark-textarea" {...register('message', { required: true })}></textarea>
+          <br/>
           {errors.message && <span className="dark-error">El mensaje es obligatorio</span>}
         </div>
 
